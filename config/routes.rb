ba894421146a52ea25b11d4520113192c7e39dd3
TakehiroKatayama <t.katayama.info@gmail.com>
2021-10-16 20:06:25 +0900

    ADD:Shopページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'terms', to: 'static_pages#terms'
   get 'privacy', to: 'static_pages#privacy'
-  get 'shop', to: 'static_pagese#shop'
+  get 'shop', to: 'static_pages#shop'
 end
