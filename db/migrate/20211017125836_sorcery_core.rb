7f6b00051557eb52b7613d001c966da32a5c7f23
TakehiroKatayama <t.katayama.info@gmail.com>
2021-10-17 23:35:45 +0900

    ADD:userモデルを作成

--- db/migrate/20211017125836_sorcery_core.rb
+++ db/migrate/20211017125836_sorcery_core.rb
@@ -1,13 +1,15 @@
 class SorceryCore < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :email,            null: false
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :phonenumber, null: false
       t.string :crypted_password
       t.string :salt
 
-      t.timestamps                null: false
+      t.timestamps null: false
     end
 
-    add_index :users, :email, unique: true
+    add_index :users, %i[email phonenumber], unique: true
   end
 end
