aafa1e5a0d0562c266ae8a0af6abab4ca5e6020e
TakehiroKatayama <t.katayama.info@gmail.com>
2021-10-17 23:52:38 +0900

    ADD:user登録機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'terms', to: 'static_pages#terms'
   get 'privacy', to: 'static_pages#privacy'
   get 'shop', to: 'static_pages#shop'
+
+  resources :users, only: %i[new create show]
 end
