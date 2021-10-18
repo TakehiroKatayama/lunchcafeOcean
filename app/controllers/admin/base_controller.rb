1a646b61bd8401b5f828c74fe7ff38316f197993
TakehiroKatayama <t.katayama.info@gmail.com>
2021-10-18 14:23:17 +0900

    ADD:管理画面にフラッシュメッセージを設定

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,12 +1,13 @@
 class Admin::BaseController < ApplicationController
-  before_action :check_admin
   layout 'admin/layouts/application'
+  before_action :check_admin
+  add_flash_types :success, :info, :warning, :danger
 
   def not_authenticated
     redirect_to admin_login_path
   end
 
   def check_admin
-    redirect_to root_path unless current_user.admin?
+    redirect_to root_path, warning: 'エラー' unless current_user.admin?
   end
 end
--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -7,14 +7,15 @@ class Admin::UserSessionsController < Admin::BaseController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_to admin_root_path
+      redirect_to admin_root_path, success: 'ログインしました'
     else
+      flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
   end
 
   def destroy
     logout
-    redirect_to admin_login_path
+    redirect_to admin_login_path, success: 'ログアウトしました'
   end
 end
