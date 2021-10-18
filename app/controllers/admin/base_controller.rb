3d2310c208ce7316f288c9d2b2219f4cf622de56
TakehiroKatayama <t.katayama.info@gmail.com>
2021-10-18 13:28:17 +0900

    ADD:管理者権限を持つユーザーのみ管理画面にログインできるよう設定

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,12 @@
 class Admin::BaseController < ApplicationController
+  before_action :check_admin
   layout 'admin/layouts/application'
+
+  def not_authenticated
+    redirect_to admin_login_path
+  end
+
+  def check_admin
+    redirect_to root_path unless current_user.admin?
+  end
 end
