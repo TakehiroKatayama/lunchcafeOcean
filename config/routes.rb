dc71c7b37e20e2c8dcd0871e620a42aa26269fa6
TakehiroKatayama <t.katayama.info@gmail.com>
2021-10-18 10:37:06 +0900

    ADD:マイページを設置

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get 'privacy', to: 'static_pages#privacy'
   get 'shop', to: 'static_pages#shop'
 
-  resources :users, only: %i[new create show]
+  resources :users, only: %i[new create]
+  get '/mypage', to: 'users#show'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
