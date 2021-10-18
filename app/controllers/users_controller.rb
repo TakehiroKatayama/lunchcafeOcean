2595816b559cb92536b3addcc6ec4cbed942ff8d
TakehiroKatayama <t.katayama.info@gmail.com>
2021-10-18 10:43:03 +0900

    ADD:アカウント編集機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,19 @@ class UsersController < ApplicationController
 
   def show; end
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to '/mypage'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[new create]
   get '/mypage', to: 'users#show'
+  get '/mypage/edit', to: 'users#edit'
+  patch '/mypage/edit', to: 'users#update'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
