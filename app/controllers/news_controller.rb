689ea360fa4e3690df242cc88976f351234be238
TakehiroKatayama <t.katayama.info@gmail.com>
2021-10-20 00:19:46 +0900

    ADD:news詳細画面設定

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,4 +2,9 @@ class NewsController < ApplicationController
   def index
     @news = News.order(created_at: 'desc').page(params[:page])
   end
+
+  def show
+    @news = News.find(params[:id])
+    @recent_news = News.order(created_at: 'desc').first(5)
+  end
 end
