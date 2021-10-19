8c197155e5aedc2d43e335965d7829dd43ffe41e
TakehiroKatayama <t.katayama.info@gmail.com>
2021-10-19 23:51:50 +0900

    ADD:newsの管理画面投稿機能を追加

--- app/controllers/admin/news_controller.rb
+++ app/controllers/admin/news_controller.rb
@@ -1,15 +1,31 @@
 class Admin::NewsController < Admin::BaseController
-  def index; end
+  def index
+    @news = News.order(created_at: 'desc').page(params[:page])
+  end
 
   def show; end
 
-  def new; end
+  def new
+    @news = News.new
+  end
 
-  def create; end
+  def create
+    @news = News.create(news_params)
+    if @news.save
+      redirect_to admin_news_index_path, success: 'ニュースを投稿しました。'
+    else
+      flash.now['danger'] = 'ニュースの投稿に失敗しました。'
+      render :new
+    end
+  end
 
   def edit; end
 
   def update; end
 
   def destroy; end
+
+  def news_params
+    params.require(:news).permit(:title, :body, :category)
+  end
 end
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,2 +1,8 @@
 class News < ApplicationRecord
+  enum category: {
+    info: 0,
+    event: 1,
+    menu: 2,
+    campaign: 3
+  }
 end
