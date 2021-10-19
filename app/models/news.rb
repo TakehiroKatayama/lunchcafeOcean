fd07109bd9da7400193c724a771c3ff9da685589
TakehiroKatayama <t.katayama.info@gmail.com>
2021-10-20 00:44:50 +0900

    ADD:news詳細ページに投稿の前後移動のボタンを設置

--- app/models/news.rb
+++ app/models/news.rb
@@ -7,4 +7,12 @@ class News < ApplicationRecord
     menu: 2,
     campaign: 3
   }
+
+  def previous
+    News.where('id < ?', id).order('id desc').first
+  end
+
+  def next
+    News.where('id > ?', id).order('id asc').first
+  end
 end
