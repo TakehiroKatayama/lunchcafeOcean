940b38b20ca68187c3631f6b8dd13f2a526dcaeb
TakehiroKatayama <t.katayama.info@gmail.com>
2021-10-20 00:45:39 +0900

    ADD:newsモデルにバリデーションを追加

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,6 +1,10 @@
 class News < ApplicationRecord
   mount_uploader :news_image, NewsImageUploader
 
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :body, presence: true, length: { maximum: 5000 }
+  validates :category, presence: true
+
   enum category: {
     info: 0,
     event: 1,
