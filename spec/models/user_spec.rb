5b24f1f971f206735c787f5d707a9d5deef08cdc
TakehiroKatayama <t.katayama.info@gmail.com>
2021-10-28 23:49:21 +0900

    ADD:userのモデルスペックを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'ユーザー名、メールアドレス、電話番号、パスワードがある場合、有効' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+  end
 end
