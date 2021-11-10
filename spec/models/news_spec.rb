76a536b679d5a9644d63e2b24df5998bb8dcf12a
TakehiroKatayama <t.katayama.info@gmail.com>
2021-11-10 23:42:01 +0900

    ADD:Newsのモデルスペックを追加

--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe News, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'タイトル、内容、カテゴリーがある場合、有効' do
+      news = build(:news)
+      expect(news).to be_valid
+    end
+  end
 end
