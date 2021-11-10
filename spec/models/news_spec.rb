69b0c21ab67f15f92c5b56cab99d26510f2fc52f
TakehiroKatayama <t.katayama.info@gmail.com>
2021-11-10 23:44:42 +0900

    ADD:Newsのtitleについてのモデルスペックを追加

--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -6,5 +6,17 @@ RSpec.describe News, type: :model do
       news = build(:news)
       expect(news).to be_valid
     end
+
+    it 'タイトルがない場合、無効' do
+      news = build(:news, title: nil)
+      news.valid?
+      expect(news.errors[:title]).to include('を入力してください')
+    end
+
+    it 'タイトルが21文字以上、無効' do
+      news = build(:news, title: 'a' * 21)
+      news.valid?
+      expect(news.errors[:title]).to include('は20文字以内で入力してください')
+    end
   end
 end
