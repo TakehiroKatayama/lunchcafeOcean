9be9f1f8193c44cb45c3590340c85fa84a8d8822
TakehiroKatayama <t.katayama.info@gmail.com>
2021-11-17 16:37:19 +0900

    ADD:管理者画面user編集機能を追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,28 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: %i[edit update]
+
   def index
     @users = User.all.order(created_at: :desc)
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, success: 'アカウント情報を編集しました'
+    else
+      flash.now['danger'] = 'アカウント情報の変更に失敗しました'
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :phonenumber, :role)
   end
 end
