28bc4490b12f473a5877bbf2ad279413bba1e524
TakehiroKatayama <t.katayama.info@gmail.com>
2021-11-22 22:18:23 +0900

    ADD:Reservationモデルのカラムにenumを追加

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,20 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :capacity
+
+  enum visiting_time: {
+    '11:00': 0,
+    '11:30': 1,
+    '12:00': 2,
+    '12:30': 3,
+    '13:00': 4,
+    '13:30': 5,
+    '14:00': 6
+  }
+
+  enum reservation_status: {
+    visiting: 0,
+    visited: 1,
+    cancel: 2
+  }
 end
