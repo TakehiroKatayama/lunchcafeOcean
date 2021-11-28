800fdb24f49e0c8dcc5ce019e6a902c9350bb2b2
TakehiroKatayama <t.katayama.info@gmail.com>
2021-11-28 22:44:21 +0900

    ADD:コントローラーとビューを作成

---
+++ app/controllers/admin/reservations_controller.rb
@@ -0,0 +1,13 @@
+class Admin::ReservationsController < Admin::BaseController
+  def index; end
+
+  def new; end
+
+  def create; end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+end
