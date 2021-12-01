e2b6ee20fbdecb5671c87ed75a1b89c26f770671
TakehiroKatayama <t.katayama.info@gmail.com>
2021-12-01 23:58:29 +0900

    ADD:管理画面に新規予約機能を追加

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -7,9 +7,21 @@ class Admin::ReservationsController < Admin::BaseController
     @reservation = Reservation.find(params[:id])
   end
 
-  def new; end
+  def new
+    @reservation = Reservation.new
+  end
 
-  def create; end
+  def create
+    Reservation.transaction do
+      capacity_id = Capacity.find_by(start_time: params[:reservation][:capacity_id]).id
+      @reservation = Reservation.create!(reservation_params.merge(capacity_id: capacity_id))
+      @reservation.capacity.update!(remaining_seat: @reservation.capacity.remaining_seat - @reservation.number_of_people)
+      redirect_to admin_reservations_path, success: '予約が完了しました'
+    end
+  rescue StandardError
+    flash.now['danger'] = "予約ができませんでした \n 今日以降の定休日以外の日付を入力してください \n 予約日の席数を確認してください"
+    render :new
+  end
 
   def edit; end
 
