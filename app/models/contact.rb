3097cd248c0c7cfa6725b75a9a93735b1509916a
TakehiroKatayama <t.katayama.info@gmail.com>
2021-12-14 23:54:58 +0900

    ADD:contactモデルのバリデーションを追加

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,11 @@
 class Contact < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  VALID_PHONE_REGEX = /\A\d{10,11}\z/.freeze
+
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, length: { maximum: 256 }
+  validates :phonenumber, presence: true, format: { with: VALID_PHONE_REGEX }, length: { in: 10..11 }
+  validates :message, presence: true, length: { maximum: 2000 }
+  validates :category, presence: true
+  validates :status, presence: true
 end
