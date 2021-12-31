8c8f3a2381085cec3c2749fa35f7b0fff12cf96f
TakehiroKatayama <t.katayama.info@gmail.com>
2021-12-31 21:56:00 +0900

    ADD:お問い合わせ管理画面indexを作成

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,7 +1,7 @@
-class Admin::ContactsController < ApplicationController
+class Admin::ContactsController < Admin::BaseController
   def index
+    @contacts = Contact.all.order(created_at: 'desc')
   end
 
-  def show
-  end
+  def show; end
 end
