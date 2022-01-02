5ca0a4600d1d232f5e6370b7eef7a64caece6727
TakehiroKatayama <t.katayama.info@gmail.com>
2022-01-02 23:48:04 +0900

    ADD:お問い合わせ管理画面showにステータス更新機能を追加

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -6,4 +6,18 @@ class Admin::ContactsController < Admin::BaseController
   def show
     @contact = Contact.find(params[:id])
   end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update!(contact_params)
+      redirect_to admin_contacts_path, success: 'ステータスを更新しました'
+    else
+      flash.now[:danger] = '対応ステータスを更新できませんでした'
+      render :show
+    end
+  end
+
+  def contact_params
+    params.require(:contact).permit(:status)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
     resources :news
     resources :users, only: %i[index show edit update destroy]
     resources :reservations
-    resources :contacts, only: %i[index show]
+    resources :contacts, only: %i[index show update]
     patch '/reservations/cancel/:id', to: 'reservations#cancel'
     resources :capacities, only: %i[index show edit update]
   end
