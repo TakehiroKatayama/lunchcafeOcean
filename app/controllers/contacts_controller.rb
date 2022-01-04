66c53e72fa78fadef5c9777037a86d336af89c6b
TakehiroKatayama <t.katayama.info@gmail.com>
2022-01-04 22:17:28 +0900

    FIX:お問い合わせが送信できないエラーを修正

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -16,6 +16,6 @@ class ContactsController < ApplicationController
   private
 
   def contact_params
-    params.require(:contact).permit(:name, :email, :phonenumber, :message)
+    params.require(:contact).permit(:name, :email, :phonenumber, :category, :message)
   end
 end
