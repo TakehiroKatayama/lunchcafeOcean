e4ebcceecde71b47d26fbbba52ea2743a8ae4400
TakehiroKatayama <t.katayama.info@gmail.com>
2022-01-05 23:50:49 +0900

    ADD:dashdoardにお問い合わせ一覧を表示

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,5 +1,6 @@
 class Admin::DashboardsController < Admin::BaseController
   def index
     @reservations = Reservation.all.order(created_at: 'desc')
+    @contacts = Contact.all.order(created_at: 'desc')
   end
 end
