aae56c5ec3439bc5acdd393845b3249209244176
TakehiroKatayama <t.katayama.info@gmail.com>
2022-01-08 21:11:53 +0900

    ADD:menuの投稿機能を追加

--- app/controllers/admin/menus_controller.rb
+++ app/controllers/admin/menus_controller.rb
@@ -2,4 +2,24 @@ class Admin::MenusController < Admin::BaseController
   def index
     @menus = Menu.order(category: 'asc').all
   end
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.create(menu_params)
+    if @menu.save
+      redirect_to admin_menus_path, success: 'メニューを追加しました'
+    else
+      flash.now['danger'] = 'メニューの追加に失敗しました'
+      render :new
+    end
+  end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:name, :content, :menu_category)
+  end
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,8 @@
 class Menu < ApplicationRecord
+  enum category: {
+    lunch: 0,
+    cake: 1,
+    wholecake: 2,
+    others: 3
+  }
 end
