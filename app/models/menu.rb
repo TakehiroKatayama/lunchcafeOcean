503c0586aeb86a661d86e9ccb189c3e241a41b49
TakehiroKatayama <t.katayama.info@gmail.com>
2022-01-10 17:03:43 +0900

    ADD:menuのバリデーションを設定

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,11 @@
 class Menu < ApplicationRecord
   mount_uploader :menu_image, MenuImageUploader
 
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :content, length: { maximum: 100 }
+  validates :menu_category, presence: true
+  validates :menu_image, presence: true
+
   enum menu_category: {
     lunch: 0,
     cake: 1,
