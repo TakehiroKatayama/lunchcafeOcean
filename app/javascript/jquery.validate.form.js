b7afce1f7430694265fa5f86604776cc6aa77451
TakehiroKatayama <t.katayama.info@gmail.com>
2022-01-13 15:26:14 +0900

    ADD:reservationフォームのバリデーションを設定

--- app/javascript/jquery.validate.form.js
+++ app/javascript/jquery.validate.form.js
@@ -49,5 +49,58 @@ $(document).on('turbolinks:load', function () {
         },
       },
     });
+    $('#reservation_form').validate({
+      errorElement: 'span',
+      rules: {
+        'reservation[name]': {
+          required: true,
+          maxlength: 20,
+        },
+        'reservation[email]': {
+          required: true,
+          email: true,
+          maxlength: 256,
+        },
+        'reservation[phonenumber]': {
+          required: true,
+          rangelength: [10, 11],
+          number: true,
+        },
+        'reservation[capacity_id]': {
+          required: true,
+        },
+        'reservation[visiting_time]': {
+          required: true,
+        },
+        'reservation[number_of_people]': {
+          required: true,
+        },
+      },
+      messages: {
+        'reservation[name]': {
+          required: 'お名前を入力してください',
+          maxlength: '20文字以内で入力してください',
+        },
+        'reservation[email]': {
+          required: 'メールアドレスを入力して下さい',
+          email: 'メールアドレスの形式で入力してください',
+          maxlength: '256文字以内で入力してください',
+        },
+        'reservation[phonenumber]': {
+          required: '電話番号を半角数字で入力して下さい',
+          rangelength: '10〜11文字で入力してください',
+          number: 'ハイフンなしで入力して下さい',
+        },
+        'reservation[capacity_id]': {
+          required: '日付を選択して下さい',
+        },
+        'reservation[visiting_time]': {
+          required: '来店時間を選択して下さい',
+        },
+        'reservation[number_of_people]': {
+          required: '人数を選択して下さい',
+        },
+      },
+    });
   });
 });
