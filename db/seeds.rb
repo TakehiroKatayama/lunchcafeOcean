44280b87f29103020cb51c07d744e5c6124b0c2e
TakehiroKatayama <t.katayama.info@gmail.com>
2022-01-31 23:59:00 +0900

    FIX:本番環境設定を変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# User.create!(name: '管理者', email: 'admin@example.com', phonenumber: '0700000000', password: 'test1111', password_confirmation: 'test1111', role: 1)
+User.create!(name: '管理者', email: 'admin@example.com', phonenumber: '0700000000', password: 'test1111', password_confirmation: 'test1111', role: 1)
 puts '管理者のテストデータ挿入'
 
-30.times do |i|
+1000.times do |i|
   date = Date.today + i
   Capacity.create(start_time: date)
 end
