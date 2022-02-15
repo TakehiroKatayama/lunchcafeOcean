c1d33fdd56802800cbede4d7adb148494ed90cba
TakehiroKatayama <t.katayama.info@gmail.com>
2022-02-15 22:58:08 +0900

    FIX:新着メソッドを修正

--- app/models/concerns/recent.rb
+++ app/models/concerns/recent.rb
@@ -2,7 +2,7 @@ module Recent
   extend ActiveSupport::Concern
 
   included do
-    scope :one_week, -> { where(created_at: 1.week.ago..Time.zone.today).order(created_at: 'desc') }
+    scope :one_week, -> { where(created_at: 1.week.ago..Time.zone.now).order(created_at: 'desc') }
     # 作成から一週間以内のものを降順にで取得する
   end
 end
