403560bc961a8e1240c843f84ec171fa3b392920
TakehiroKatayama <t.katayama.info@gmail.com>
2022-02-18 15:51:48 +0900

    FIX:menuのスペルミスを修正

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,7 +1,7 @@
 class MenusController < ApplicationController
   def index
     @lunchs = Menu.where(menu_category: 'lunch')
-    @sweats = Menu.where(menu_category: 'cake')
+    @sweets = Menu.where(menu_category: 'cake')
     @wholecakes = Menu.where(menu_category: 'wholecake')
     @others = Menu.where(menu_category: 'others')
   end
