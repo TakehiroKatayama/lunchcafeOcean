79f7ad5d465a579708792bb132c3bc1e2c85ab27
TakehiroKatayama <t.katayama.info@gmail.com>
2022-02-21 23:06:58 +0900

    FIX:newsテーブルのbodyを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_20_073147) do
+ActiveRecord::Schema.define(version: 2022_02_21_140227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +75,6 @@ ActiveRecord::Schema.define(version: 2022_02_20_073147) do
 
   create_table "news", force: :cascade do |t|
     t.string "title", null: false
-    t.text "body", null: false
     t.integer "category", null: false
     t.string "news_image"
     t.datetime "created_at", precision: 6, null: false
