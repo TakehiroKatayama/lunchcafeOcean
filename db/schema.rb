a512109fc19f4d08d07af29b7f2f56286bf1fc11
TakehiroKatayama <t.katayama.info@gmail.com>
2022-04-21 22:05:10 +0900

    ADD:capacitiesテーブルに予約日のステータスのカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_140227) do
+ActiveRecord::Schema.define(version: 2022_04_21_130242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2022_02_21_140227) do
     t.integer "remaining_seat", default: 20, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "capacity_status", default: 0
   end
 
   create_table "contacts", force: :cascade do |t|
