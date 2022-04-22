7dac9c28e280e53f9f30ebb83d4df4dee82b04d5
TakehiroKatayama <t.katayama.info@gmail.com>
2022-04-22 23:20:19 +0900

    ADD:capacitiesテーブルにステータスカラムのenumを設定

--- app/models/capacity.rb
+++ app/models/capacity.rb
@@ -2,4 +2,10 @@ class Capacity < ApplicationRecord
   has_many :reservations
 
   validates :remaining_seat, numericality: { greater_than_or_equal_to: 0 }
+
+  enum capacity_status: {
+    vacancy: 0,
+    full: 1,
+    closed: 2
+  }
 end
