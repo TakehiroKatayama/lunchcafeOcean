849cb011b0a3d05cbbdbc3df6f71b1d6e22e224a
TakehiroKatayama <t.katayama.info@gmail.com>
2022-05-03 16:28:48 +0900

    ADD:gem exception_notificationをインストール

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ puts '管理者のテストデータ挿入'
 
 1000.times do |i|
   date = Date.today + i
-  Capacity.create(start_time: date)
 end
 puts 'Capacityのテストデータ挿入'
+
+1000.times { |i| Capacity.create(start_time: Date.today + i) }
